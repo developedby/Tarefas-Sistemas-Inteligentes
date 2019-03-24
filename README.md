2160a8374c7d1b317064db581101f829e511c015
Nicolas Abril <nicolas.abril@gmail.com>
2019-03-24 14:11:26 -0300

    Modifica readme, adiciona gitignore e t01

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Tarefas de Sistemas Inteligentes
 
-Tareafs da Disciplina Sistemas Inteligentes, do curso de Engenharia de Computação UTFPR Curitiba - 2019.1 - Pro. Tacla
+Tareafs da Disciplina Sistemas Inteligentes, do curso de Engenharia de Computação UTFPR Curitiba - 2019.1 - Prof. Tacla
+
+Arquivos Base por Cesar Tacla
+Modificações (implementação das tarefas) por Nicoals Abril e Lucca Rawlyk
---
+++ t01/src/sistema/Agente.java
@@ -0,0 +1,74 @@
+package sistema;
+
+import problema.*;
+import ambiente.*;
+import arvore.TreeNode;
+import arvore.fnComparator;
+import comuns.*;
+import static comuns.PontosCardeais.*;
+import java.util.ArrayList;
+
+ import java.util.Random; 
+
+/**
+ *
+ * @author tacla
+ * @author Nicolas Abril
+ */
+public class Agente implements PontosCardeais {
+    /* referência ao ambiente para poder atuar no mesmo*/
+    Model model;
+    Estado estAtu; // guarda o estado atual (posição atual do agente)
+    int plan[];
+    double custo;
+    static int ct = -1;
+           
+    Random rand;
+
+    public Agente(Model m) {
+        this.model = m; 
+        // Posiciona o agente fisicamente no ambiente na posicao inicial
+        model.setPos(8,0);
+
+        rand = new Random();
+    }
+    
+     /**
+     * Agente escolhe qual acao será executada em um ciclo de raciocinio.
+     * Observar que o agente executa somente uma acao por ciclo.
+     */
+    public int deliberar() {               
+        //  contador de acoes
+        this.ct++;
+        // @todo T1: perceber por meio do sensor a posicao atual e imprimir
+        Estado estado = sensorPosicao();
+        System.out.println("Posição na ação numero " + this.ct + ": " + estado.getString());
+        // @todo T1: a cada acao escolher uma acao {N, NE, L, SE, S, SO, O, NO}
+        executarIr(rand.nextInt(8)); //executar a acao escolhida
+        
+        return 1; // Se retornar -1, encerra o agente
+    }
+
+    /**
+    * Atuador: executa 'fisicamente' a acao Ir
+    * @param direcao um dos pontos cardeais
+    */
+    public int executarIr(int direcao) {
+        //@todo T1 - invocar metodo do Model - atuar no ambiente 
+        this.model.ir(direcao);
+        return 1; // deu certo
+    }
+
+    /**
+     * Simula um sensor que realiza a leitura da posição atual no ambiente e
+     * traduz para um par de coordenadas armazenadas em uma instância da classe
+     * Estado.
+     * @return Estado contendo a posição atual do agente no labirinto 
+     */
+    public Estado sensorPosicao() {
+        //@todo T1 - sensor deve ler a posicao do agente no labirinto (environment)
+        int pos[] = this.model.lerPos();
+        return new Estado(pos[0],pos[1]);
+    }
+    
+}
