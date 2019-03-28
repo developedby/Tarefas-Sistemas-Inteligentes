a500e660c971095509f69d9e73581dddfb63a231
Nicolas Abril <nicolas.abril@gmail.com>
2019-03-28 18:44:55 -0300

    Implementa agente exceto imprimir

--- t02/sistema/Agente.java
+++ t02/sistema/Agente.java
@@ -4,10 +4,13 @@ import ambiente.*;
 import problemaEnuncT02.*;
 import comuns.*;
 import static comuns.PontosCardeais.*;
+import java.util.*;
 
 /**
  *
  * @author tacla
+ * @author Nicolas Abril
+ * @author Lucca Rawlyk
  */
 public class Agente implements PontosCardeais {
     /* referência ao ambiente para poder atuar no mesmo*/
@@ -17,20 +20,36 @@ public class Agente implements PontosCardeais {
     /* @todo T2: fazer uma sequencia de acoes a ser executada em deliberar
        e armazena-la no atributo plan[]
     */
-    int plan[];
+    int plan[] = {N,N,N,NE,E,E,E,E,E,E,NE,N};
     double custo;
     static int ct = -1;
            
     public Agente(Model m) {
         this.model = m;
         prob = new Problema();
-        
         //@todo T2: Aqui vc deve preencher a formulacao do problema 
-        
+        custo = 0;
         //@todo T2: crencas do agente a respeito do labirinto
         prob.criarLabirinto(9, 9);
-               
+        prob.labir.porParedeVertical(0, 1, 0);
+        prob.labir.porParedeVertical(0, 0, 1);
+        prob.labir.porParedeVertical(5, 8, 1);
+        prob.labir.porParedeVertical(5, 5, 2);
+        prob.labir.porParedeVertical(8, 8, 2);
+        prob.labir.porParedeHorizontal(4, 7, 0);
+        prob.labir.porParedeHorizontal(7, 7, 1);
+        prob.labir.porParedeHorizontal(3, 5, 2);
+        prob.labir.porParedeHorizontal(3, 5, 3);
+        prob.labir.porParedeHorizontal(7, 7, 3);
+        prob.labir.porParedeVertical(6, 7, 4);
+        prob.labir.porParedeVertical(5, 6, 5);
+        prob.labir.porParedeVertical(5, 7, 7);
+
         //@todo T2: crencas do agente: Estado inicial, objetivo e atual
+        prob.defEstIni(8,0);
+        prob.defEstObj(2,8);
+        estAtu = prob.estIni;
+
         // utilizar atributos da classe Problema
         
     }
@@ -39,16 +58,37 @@ public class Agente implements PontosCardeais {
      * @return 1 enquanto o plano não acabar; -1 quando acabar
      */
     public int deliberar() {
+        // Incrementa contador de ações
         ct++;
-        //@todo T2: executar o plano de acoes: SOMENTE UMA ACAO POR CHAMADA DESTE METODO
-        // Ao final do plano, verifique se o agente atingiu o estado objetivo verificando
-        // com o teste de objetivo
         
-        
-        //@todo T2: imprimir o que foi pedido
+        // Executa a proxima acao do plano
+        executarIr(plano[ct]);
 
+        // Incrementa o custo acumulado
+        if (Arrays.asList(N,L,S,O).contains(plano[ct])) {
+            custo += 1;
+        }
+        else {
+            custo += 1.5;
+        }
         
-        return 1;
+        // Imprime o que foi pedido
+        
+
+        // Verifica se terminou
+        if (ct >= plan.length-1) {
+            // Se terminou, verifica se alcançou o objetivo
+            if (prob.testeObjetivo(sensorPosicao())) {
+                System.out.println("\n\nO agente alcançou o objetivo!");
+            }
+            else {
+                System.out.println("\n\nO agente não alcançou o objetivo :(");
+            }
+            return -1;
+        }
+        else {
+            return 1;
+        }
     }
     
     /**Funciona como um driver ou um atuador: envia o comando para
@@ -66,6 +106,38 @@ public class Agente implements PontosCardeais {
         int pos[];
         pos = model.lerPos();
         return new Estado(pos[0], pos[1]);
+    }
+
+    public Estado suc(Estado est, int acao) {
+        if (prob.acoesPossiveis(est)[acao] == 1) {
+            if (acao == N){
+                return new Estado(est.getLin()-1, est.getCol());
+            }
+            else if (acao == NE){
+                return new Estado(est.getLin()-1, est.getCol()+1);
+            }
+            else if (acao == L){
+                return new Estado(est.getLin(), est.getCol()+1);
+            }
+            else if (acao == SE){
+                return new Estado(est.getLin()+1, est.getCol()+1);
+            }
+            else if (acao == S){
+                return new Estado(est.getLin()+1, est.getCol());
+            }
+            else if (acao == SO){
+                return new Estado(est.getLin()+1, est.getCol()-1);
+            }
+            else if (acao == O){
+                return new Estado(est.getLin(), est.getCol()-1);
+            }
+            else if (acao == NO){
+                return new Estado(est.getLin()-1, est.getCol()-1);
+            }
+        }
+        else {
+            return est;
+        }
     }
 }    
 
